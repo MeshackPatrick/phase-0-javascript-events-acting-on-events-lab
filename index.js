b1bc8dbfcbe50ac03d1dc8d2f0d031d4b6715e9b
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-26 11:07:02 +0300

    moving dodger

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function moveDodgerLeft() {
+    var dodger = document.getElementById('dodger');
+    var left = parseInt(dodger.style.left);
+    var newLeft = left - 1; 
+    dodger.style.left = newLeft + 'px';
+  }
+  
+  function moveDodgerRight() {
+    var dodger = document.getElementById('dodger');
+    var left = parseInt(dodger.style.left);
+    var newLeft = left + 1; 
+    dodger.style.left = newLeft + 'px';
+  }
+  
